5d5fbab4d5af865d61065db45a3a9eb2f88cd118
Russell Troxel <russell.troxel@rackspace.com>
2016-06-30 20:08:28 -0500

    celerize - initial commit.

--- app.py
+++ app.py
@@ -4,35 +4,22 @@
 from __future__ import print_function
 
 import argparse
-import certifi
 import logging
-import netaddr
 import sys
-import socket
 import time
 import yaml
 
-from datetime import datetime
-
 from mongoengine import connect
-from elasticsearch import Elasticsearch
 
-from donthackme_elasticsearch.indexes import donthackme_mappings
 from donthackme_elasticsearch.models import (collection_map,
-                                             TransactionLog,
-                                             IpLocation)
-
-
+                                             TransactionLog)
+from donthackme_elasticsearch.tasks import (process_object,
+                                            process_trans_log)
 from pymongo import CursorType
 
-from celery import Celery
-
-from celery.contrib.methods import task_method
 
 logging.basicConfig(format="%(asctime)s - %(levelname)s : %(message)s",
                     level=logging.INFO)
-
-app = Celery("app", broker="redis://localhost:6379/0")
 
 
 def config(configfile):
@@ -48,17 +35,6 @@ def config(configfile):
         return conf
 
 
-def geoip(ip_address):
-    """Geolocate IP."""
-    ip_address = int(netaddr.IPAddress(ip_address))
-    location = IpLocation.objects.get(
-        ip_from__lte=ip_address,
-        ip_to__gt=ip_address
-    )
-
-    return location
-
-
 class TransLogReader(object):
     """Read TransactionLogs from MongoEngine."""
 
@@ -70,78 +46,6 @@ class TransLogReader(object):
 
         self.db = TransactionLog._get_db()
         self.logs = self.db.transaction_log
-
-        es_config = self.prepare_es_config(self.conf["elasticsearch"])
-        self.es = Elasticsearch(**es_config)
-
-        self.index = self.conf["index"]
-        if not self.es.indices.exists(self.index):
-            self.es.indices.create(
-                index=self.index,
-                body=donthackme_mappings
-            )
-
-    def _ensure_ip(self, address):
-        try:
-            socket.inet_aton(address)
-            return address
-        except:
-            return socket.gethostbyname(address)
-
-    def prepare_es_config(self, es_config):
-        """Prepare elasticsearch config entry for use in py-elasticsearch."""
-        es_config["http_auth"] = (
-            es_config.pop("username"),
-            es_config.pop("password")
-        )
-        es_config["ca_certs"] = certifi.where()
-        return es_config
-
-    @app.task(filter=task_method)
-    def process_trans_log(self, log):
-        """Process a TransactionLog object."""
-        doc_class = collection_map[log["collection"]]
-        obj = doc_class.objects.get(id=log["doc_id"])
-        self.process_object(log["collection"], obj)
-
-    @app.task(filter=task_method)
-    def process_object(self, collection_name, obj):
-        """Process a MongoEngine object."""
-        item = obj.to_dict()
-        item["doc_id"] = str(obj.id)
-        if collection_name == "sensor":
-            location = geoip(item["ip"])
-            item["country"] = location.country_name
-            item["region"] = location.region_name
-            item["city"] = location.city_name
-            item["location"] = {
-                "lat": location.lat,
-                "lon": location.lon
-            }
-        if collection_name == "session":
-            item["timestamp"] = item["start_time"]
-            location = geoip(item["source_ip"])
-            item["country"] = location.country_name
-            item["region"] = location.region_name
-            item["city"] = location.city_name
-            item["location"] = {
-                "lat": location.lat,
-                "lon": location.lon
-            }
-        print("{0}:  {1}  -  {2}".format(
-            str(datetime.utcnow()),
-            collection_name,
-            item["doc_id"]
-        ))
-        for key in ["source_ip", "dest_ip"]:
-            if key in item:
-                item[key] = self._ensure_ip(item[key])
-        self.es.index(
-            index=self.index,
-            doc_type=collection_name,
-            id=item["doc_id"],
-            body=item
-        )
 
     def get_cursor(self, last_id=-1, await_data=True):
         """Create cursor object based on last_id."""
@@ -158,7 +62,7 @@ class TransLogReader(object):
             cur = self.get_cursor(last_id)
             for msg in cur:
                 last_id = msg['ts']
-                self.process_trans_log.delay(msg)
+                process_trans_log.delay(msg)
             time.sleep(0.1)
 
     def import_collection(self, collection_name):
@@ -166,7 +70,7 @@ class TransLogReader(object):
         doc_class = collection_map[collection_name]
         cur = doc_class.objects()
         for msg in cur:
-            self.process_object.delay(collection_name, msg)
+            process_object.delay(collection_name, msg)
 
 
 if __name__ == "__main__":
