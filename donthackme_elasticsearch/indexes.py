31c11bceba8f16fed7b98f27f436587b4a6db7bf
Russell Troxel <russell.troxel@rackspace.com>
2016-07-01 08:16:45 -0500

    add .raw fields to mappings

--- donthackme_elasticsearch/indexes.py
+++ donthackme_elasticsearch/indexes.py
@@ -14,107 +14,214 @@
 # under the License.
 
 donthackme_mappings = {
-  "mappings": {
-    "sensor": {
-      "properties": {
-        "timestamp":    { "type": "date"      },
-        "name":         { "type": "string"    },
-        "ip":           { "type": "ip"        },
-        "country":      { "type": "string"    },
-        "region":       { "type": "string"    },
-        "city":         { "type": "string"    },
-        "location":     { "type": "geo_point" }
-      }
-    },
-    "command": {
-      "properties": {
-        "timestamp":    { "type": "date"      },
-        "sensor_name":  { "type": "string"    },
-        "sensor_ip":    { "type": "ip"        },
-        "command":		{ "type": "string"    },
-        "success":      { "type": "boolean"   }
-      }
-    },
-    "credentials": {
-      "properties": {
-        "timestamp":    { "type": "date"      },
-        "sensor_name":  { "type": "string"    },
-        "sensor_ip":    { "type": "ip"        },
-        "username":     { "type": "string"    },
-        "password":     { "type": "string"    },
-        "success":      { "type": "boolean"   },
-      }
-    },
-    "fingerprint": {
-      "properties": {
-        "timestamp":    { "type": "date"      },
-        "sensor_name":  { "type": "string"    },
-        "sensor_ip":    { "type": "ip"        },
-        "username":     { "type": "string"    },
-        "fingerprint":  { "type": "string"    }
-      }
-    },
-    "download": {
-      "properties": {
-        "timestamp":    { "type": "date"      },
-        "sensor_name":  { "type": "string"    },
-        "sensor_ip":    { "type": "ip"        },
-        "realm":        { "type": "string"    },
-        "shasum":       {
-          "type": "string",
-          "index": "not_analyzed"
-        },
-        "url":          { "type": "string"    },
-        "outfile":      {
-          "type": "string",
-          "index": "not_analyzed"
-        }
-      }
-    },
-    "tcp_connection": {
-      "properties": {
-        "timestamp":    { "type": "date"      },
-        "sensor_name":  { "type": "string"    },
-        "sensor_ip":    { "type": "ip"        },
-        "dest_ip":		{ "type": "ip"        },
-        "dest_port":    { "type": "integer"   }
-      }
-    },
-    "session": {
-      "properties": {
-        "timestamp":    { "type": "date"      },
-        "start_time":   { "type": "date"      },
-        "end_time":     { "type": "date"      },
-        "sensor_name":  { "type": "string"    },
-        "sensor_ip":    { "type": "ip"        },
-        "source_ip":    { "type": "ip"        },
-        "src_country":  { "type": "string"    },
-        "src_region":   { "type": "string"    },
-        "src_city":         { "type": "string"    },
-        "src_location": { "type": "geo_point" },
-        "ttylog":       {
-          "type": "nested",
-          "properties": {
-            "size":         { "type": "integer" },
-            "log_location": {
-              "type": "string",
-              "index": "not_analyzed"
-            },
-            "log_binary":   {
-              "type": "binary",
-              "index": "not_analyzed"
+    "mappings": {
+        "sensor": {
+            "properties": {
+                "timestamp": {"type": "date"},
+                "name": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "ip": {"type": "ip"},
+                "country": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "region": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "city": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "location": {"type": "geo_point"}
             }
-          }
         },
-        "ttysize": {
-          "type": "nested",
-          "properties": {
-            "width":        { "type": "integer" },
-            "height":       { "type": "integer" }
-          }
+        "command": {
+            "properties": {
+                "timestamp": {"type": "date"},
+                "sensor_name": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "sensor_ip": {"type": "ip"},
+                "command": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "success": {"type": "boolean"}
+            }
         },
-        "ssh_version":  { "type": "string"    },
-      }
+        "credentials": {
+            "properties": {
+                "timestamp": {"type": "date"},
+                "sensor_name": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "sensor_ip": {"type": "ip"},
+                "username": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "password": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "success": {"type": "boolean"},
+            }
+        },
+        "fingerprint": {
+            "properties": {
+                "timestamp": {"type": "date"},
+                "sensor_name": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "sensor_ip": {"type": "ip"},
+                "username": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "fingerprint": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                }
+            }
+        },
+        "download": {
+            "properties": {
+                "timestamp": {"type": "date"},
+                "sensor_name": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "sensor_ip": {"type": "ip"},
+                "realm": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "shasum": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "url": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "outfile": {
+                    "type": "string",
+                    "index": "not_analyzed"
+                }
+            }
+        },
+        "tcp_connection": {
+            "properties": {
+                "timestamp": {"type": "date"},
+                "sensor_name": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "sensor_ip": {"type": "ip"},
+                "dest_ip": {"type": "ip"},
+                "dest_port": {"type": "integer"}
+            }
+        },
+        "session": {
+            "properties": {
+                "timestamp": {"type": "date"},
+                "start_time": {"type": "date"},
+                "end_time": {"type": "date"},
+                "sensor_name": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "sensor_ip": {"type": "ip"},
+                "source_ip": {"type": "ip"},
+                "src_country": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "src_region": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "src_city": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+                "src_location": {"type": "geo_point"},
+                "ttylog": {
+                    "type": "nested",
+                    "properties": {
+                        "size": {"type": "integer"},
+                        "log_location": {
+                            "type": "string",
+                            "index": "not_analyzed"
+                        },
+                        "log_binary": {
+                            "type": "binary",
+                            "index": "not_analyzed"
+                        }
+                    }
+                },
+                "ttysize": {
+                    "type": "nested",
+                    "properties": {
+                        "width": {"type": "integer"},
+                        "height": {"type": "integer"}
+                    }
+                },
+                "ssh_version": {
+                    "type": "string",
+                    "fields": {
+                        "raw": {"type": "string", "index": "not_analyzed"}
+                    }
+                },
+            }
+        }
     }
-  }
 }
