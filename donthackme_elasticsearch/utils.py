60813aa7a21e4627d8bf07be8abdedab8ff7198a
Russell Troxel <russell.troxel@rackspace.com>
2016-06-30 20:14:06 -0500

    celerize - initial commit.

--- donthackme_elasticsearch/utils.py
+++ donthackme_elasticsearch/utils.py
@@ -1,4 +1,18 @@
 """Util Functions for ES Push."""
+# Copyright 2016 Russell Troxel
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
 import certifi
 import sys
 import yaml
@@ -17,7 +31,7 @@ def _config(configfile):
         return conf
 
 
-def _prepare_es_config(self, es_config):
+def _prepare_es_config(es_config):
     """Prepare elasticsearch config entry for use in py-elasticsearch."""
     es_config["http_auth"] = (
         es_config.pop("username"),
