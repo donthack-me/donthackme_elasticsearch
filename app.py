ce1e137024cbf362baa2603c80dbf0bc6055966f
Russell Troxel <russell.troxel@rackspace.com>
2016-07-04 14:08:02 -0500

    remove oplog

--- app.py
+++ app.py
@@ -50,14 +50,15 @@ class TransLogReader(object):
     def get_cursor(self, last_id=-1, await_data=True):
         """Create cursor object based on last_id."""
         spec = {'ts': {'$gt': last_id}}
-        cur = self.logs.find(spec,
-                             cursor_type=CursorType.TAILABLE_AWAIT)
+        cur = self.logs.find(spec)
+                             # cursor_type=CursorType.TAILABLE_AWAIT,
+                             # oplog_replay=True)
         return cur
 
     def tail_log(self):
         """Main log tailing function."""
         logging.info("Began log tailing.")
-        last_id = 80000
+        last_id = 900000
         while True:
             cur = self.get_cursor(last_id)
             for msg in cur:
